d52a12e5d4e55e2704e83a16f0b010776f85b8f2
Linhlinh1208 <linhnguyen20050812@gmail.com>
2025-06-02 17:17:10 +0700

    hoanthanh

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,13 @@ import authRouter from './auth.router'
 import admin from './admin'
 import userAuthRouter from './user/auth.router'
 import userProfileRouter from './user/profile.route'
-import achievementRoute from './user/achievement.route'
-
+import user from './user'
 function route(app) {
     app.use('/admin', admin)
     app.use('/auth', authRouter)
     app.use('/user/auth', userAuthRouter)
     app.use('/user/profile', userProfileRouter)
-    app.use('/api/achievements', achievementRoute)
+    app.use('/user', user)
 }
 
 export default route
